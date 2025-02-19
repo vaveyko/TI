2de7e328bc2dfee1775c4847b2913e9869fc3ff0
vaveyko <zakhvey.ivan@gmail.com>
2025-02-19 10:22:46 +0300

    add ClearStr method to Form class

--- lab1/lab1/Form1.cs
+++ lab1/lab1/Form1.cs
@@ -2,6 +2,19 @@ namespace lab1;
 
 public partial class Form1 : Form
 {
+    private string ClearStr(string str)
+    {
+        char[] buff = new char[str.Length];
+        int buffInd = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (Vigener.MyAlphabet.Contains(str[i]))
+            {
+                buff[buffInd++] = str[i]; 
+            }
+        }
+        return new string(buff);
+    }
     public Form1()
     {
         InitializeComponent();
