c9ec4980d038cdf52dd430d9a54e6f5e46748c87
vaveyko <zakhvey.ivan@gmail.com>
2025-02-19 17:11:16 +0300

    full design of Vigener window complited

--- lab1/lab1/Form1.Designer.cs
+++ lab1/lab1/Form1.Designer.cs
@@ -36,25 +36,28 @@ partial class Form1
         outputTextBox = new System.Windows.Forms.TextBox();
         tabControl1 = new System.Windows.Forms.TabControl();
         vigenerTabPage = new System.Windows.Forms.TabPage();
-        plefnerTabPage = new System.Windows.Forms.TabPage();
-        folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
         openFileButton = new System.Windows.Forms.Button();
+        plefnerTabPage = new System.Windows.Forms.TabPage();
+        openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+        label1 = new System.Windows.Forms.Label();
+        label2 = new System.Windows.Forms.Label();
+        label3 = new System.Windows.Forms.Label();
         tabControl1.SuspendLayout();
         vigenerTabPage.SuspendLayout();
         SuspendLayout();
         // 
         // keyTextBox
         // 
-        keyTextBox.Location = new System.Drawing.Point(6, 65);
+        keyTextBox.Location = new System.Drawing.Point(35, 65);
         keyTextBox.Name = "keyTextBox";
-        keyTextBox.Size = new System.Drawing.Size(173, 31);
+        keyTextBox.Size = new System.Drawing.Size(215, 31);
         keyTextBox.TabIndex = 0;
         // 
         // cipherButton
         // 
-        cipherButton.Location = new System.Drawing.Point(6, 112);
+        cipherButton.Location = new System.Drawing.Point(35, 112);
         cipherButton.Name = "cipherButton";
-        cipherButton.Size = new System.Drawing.Size(173, 42);
+        cipherButton.Size = new System.Drawing.Size(215, 42);
         cipherButton.TabIndex = 1;
         cipherButton.Text = "Зашифровать";
         cipherButton.UseVisualStyleBackColor = true;
@@ -62,9 +65,9 @@ partial class Form1
         // 
         // unCipherButton
         // 
-        unCipherButton.Location = new System.Drawing.Point(6, 160);
+        unCipherButton.Location = new System.Drawing.Point(35, 160);
         unCipherButton.Name = "unCipherButton";
-        unCipherButton.Size = new System.Drawing.Size(173, 42);
+        unCipherButton.Size = new System.Drawing.Size(215, 42);
         unCipherButton.TabIndex = 2;
         unCipherButton.Text = "Расшифровать";
         unCipherButton.UseVisualStyleBackColor = true;
@@ -72,18 +75,18 @@ partial class Form1
         // 
         // inputTextBox
         // 
-        inputTextBox.Location = new System.Drawing.Point(331, 51);
+        inputTextBox.Location = new System.Drawing.Point(331, 65);
         inputTextBox.Multiline = true;
         inputTextBox.Name = "inputTextBox";
-        inputTextBox.Size = new System.Drawing.Size(310, 151);
+        inputTextBox.Size = new System.Drawing.Size(435, 167);
         inputTextBox.TabIndex = 3;
         // 
         // outputTextBox
         // 
-        outputTextBox.Location = new System.Drawing.Point(331, 221);
+        outputTextBox.Location = new System.Drawing.Point(331, 270);
         outputTextBox.Multiline = true;
         outputTextBox.Name = "outputTextBox";
-        outputTextBox.Size = new System.Drawing.Size(310, 151);
+        outputTextBox.Size = new System.Drawing.Size(435, 167);
         outputTextBox.TabIndex = 4;
         // 
         // tabControl1
@@ -93,11 +96,14 @@ partial class Form1
         tabControl1.Location = new System.Drawing.Point(-3, 1);
         tabControl1.Name = "tabControl1";
         tabControl1.SelectedIndex = 0;
-        tabControl1.Size = new System.Drawing.Size(802, 611);
+        tabControl1.Size = new System.Drawing.Size(802, 493);
         tabControl1.TabIndex = 5;
         // 
         // vigenerTabPage
         // 
+        vigenerTabPage.Controls.Add(label3);
+        vigenerTabPage.Controls.Add(label2);
+        vigenerTabPage.Controls.Add(label1);
         vigenerTabPage.Controls.Add(openFileButton);
         vigenerTabPage.Controls.Add(outputTextBox);
         vigenerTabPage.Controls.Add(unCipherButton);
@@ -107,24 +113,14 @@ partial class Form1
         vigenerTabPage.Location = new System.Drawing.Point(4, 34);
         vigenerTabPage.Name = "vigenerTabPage";
         vigenerTabPage.Padding = new System.Windows.Forms.Padding(3);
-        vigenerTabPage.Size = new System.Drawing.Size(794, 573);
+        vigenerTabPage.Size = new System.Drawing.Size(794, 455);
         vigenerTabPage.TabIndex = 0;
         vigenerTabPage.Text = "Виженер";
         vigenerTabPage.UseVisualStyleBackColor = true;
         // 
-        // plefnerTabPage
-        // 
-        plefnerTabPage.Location = new System.Drawing.Point(4, 34);
-        plefnerTabPage.Name = "plefnerTabPage";
-        plefnerTabPage.Padding = new System.Windows.Forms.Padding(3);
-        plefnerTabPage.Size = new System.Drawing.Size(794, 573);
-        plefnerTabPage.TabIndex = 1;
-        plefnerTabPage.Text = "Плейфер";
-        plefnerTabPage.UseVisualStyleBackColor = true;
-        // 
         // openFileButton
         // 
-        openFileButton.Location = new System.Drawing.Point(97, 19);
+        openFileButton.Location = new System.Drawing.Point(169, 27);
         openFileButton.Name = "openFileButton";
         openFileButton.Size = new System.Drawing.Size(81, 32);
         openFileButton.TabIndex = 5;
@@ -132,22 +128,65 @@ partial class Form1
         openFileButton.UseVisualStyleBackColor = true;
         openFileButton.Click += openFileButton_Click;
         // 
+        // plefnerTabPage
+        // 
+        plefnerTabPage.Location = new System.Drawing.Point(4, 34);
+        plefnerTabPage.Name = "plefnerTabPage";
+        plefnerTabPage.Padding = new System.Windows.Forms.Padding(3);
+        plefnerTabPage.Size = new System.Drawing.Size(794, 455);
+        plefnerTabPage.TabIndex = 1;
+        plefnerTabPage.Text = "Плейфер";
+        plefnerTabPage.UseVisualStyleBackColor = true;
+        // 
+        // openFileDialog1
+        // 
+        openFileDialog1.Filter = "(*.txt)|*.txt";
+        openFileDialog1.ReadOnlyChecked = true;
+        // 
+        // label1
+        // 
+        label1.Location = new System.Drawing.Point(35, 27);
+        label1.Name = "label1";
+        label1.Size = new System.Drawing.Size(134, 32);
+        label1.TabIndex = 6;
+        label1.Text = "Ключ";
+        // 
+        // label2
+        // 
+        label2.Location = new System.Drawing.Point(331, 27);
+        label2.Name = "label2";
+        label2.Size = new System.Drawing.Size(241, 32);
+        label2.TabIndex = 7;
+        label2.Text = "Обычный текст";
+        // 
+        // label3
+        // 
+        label3.Location = new System.Drawing.Point(331, 235);
+        label3.Name = "label3";
+        label3.Size = new System.Drawing.Size(241, 32);
+        label3.TabIndex = 8;
+        label3.Text = "Преобразованный текст";
+        // 
         // Form1
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        ClientSize = new System.Drawing.Size(800, 619);
+        ClientSize = new System.Drawing.Size(800, 494);
         Controls.Add(tabControl1);
-        Text = "Form1";
+        Text = "Шифры";
         tabControl1.ResumeLayout(false);
         vigenerTabPage.ResumeLayout(false);
         vigenerTabPage.PerformLayout();
         ResumeLayout(false);
     }
 
-    private System.Windows.Forms.Button openFileButton;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
 
-    private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+    private System.Windows.Forms.Button openFileButton;
 
     private System.Windows.Forms.TabControl tabControl1;
     private System.Windows.Forms.TabPage vigenerTabPage;
